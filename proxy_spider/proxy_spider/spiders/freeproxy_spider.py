3a83382861dcc0bb4d133da02785d5377efde70c
Iana Baraksina <baraksina.ya@gmail.com>
2025-07-16 03:07:58 +0200

    upload proxies

--- proxy_spider/proxy_spider/spiders/freeproxy_spider.py
+++ proxy_spider/proxy_spider/spiders/freeproxy_spider.py
@@ -1,5 +1,6 @@
 import base64
 import json
+import requests
 from scrapy import Spider
 from datetime import datetime, timezone
 
@@ -33,15 +34,44 @@ class FreeproxySpider(Spider):
             })
 
     def closed(self, reason):
+        with open("proxies.json", "w", encoding="utf-8") as f:
+            json.dump(self.proxies, f, ensure_ascii=False, indent=2)
+
+        results = {}
+        batch_size = 29
+        for i in range(0, len(self.proxies), batch_size):
+            proxies_slice = self.proxies[i:i + batch_size]
+            proxies_list = list(map(lambda p: f"{p['ip']}:{p['port']}", proxies_slice))
+            headers = {
+                'Content-Type': 'application/json'
+            }
+            payload = {
+                "user_id": "t_f5c3f1d8",
+                "len": len(proxies_slice),
+                "proxies": ', '.join(proxies_list)
+            }
+
+            session = requests.Session()
+            session.get("https://test-rg8.ddns.net/api/get_token")
+
+            response = session.post("https://test-rg8.ddns.net/api/post_proxies", headers=headers, json=payload)
+            if response.ok:
+                data = response.json()
+                save_id = data['save_id']
+                results[save_id] = proxies_list
+                print(response.json())
+            else:
+                print('ERROR', response.status_code, response.text)
+
+        with open("results.json", "w", encoding="utf-8") as f:
+            json.dump(results, f, ensure_ascii=False, indent=2)
+
         finish_time = datetime.now(tz=timezone.utc)
         start_time = self.crawler.stats.get_value("start_time")
         duration = finish_time - start_time
         hours, remainder = divmod(duration.total_seconds(), 3600)
         minutes, seconds = divmod(remainder, 60)
         time_str = "{:02}:{:02}:{:02}".format(int(hours), int(minutes), int(seconds))
-
-        with open("proxies.json", "w", encoding="utf-8") as f:
-            json.dump(self.proxies, f, ensure_ascii=False, indent=2)
 
         with open("time.txt", "w", encoding="utf-8") as f:
             f.write(time_str)
